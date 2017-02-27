fad4017874b9330d2b3e7fa10b2ff74021fddc01
jyllsarta <illstarred.dive@gmail.com>
2017-02-27 23:45:44 +0900

    攻撃対象を選ぶときは生きているやつをリストアップしてからランダムに選ぶ

--- js/battle.js
+++ js/battle.js
@@ -15,6 +15,7 @@ function Enemy(rank,type="normal", enchant="none"){
 	this.atk = Math.floor(Math.pow(rank-50,1.8) * 1.45) 
 	this.sld = 0
 	this.hp = Math.floor(Math.pow(rank-50,1.8) * 3)
+	this.isDead  = false
 
 	//TODO type か enchantに指定が入った際のパラメータ補正
 }
@@ -29,6 +30,7 @@ function Ally(charaname){
 	this.atk = (str + dex)/2 + Math.min(str,dex)
 	this.sld = (def + agi)/2 + Math.min(def,agi)
 	this.hp = save.status[charaname].hp
+	this.isDead  = false
 
 }
 
@@ -45,6 +47,60 @@ function calcDamage(from, to){
 	var damage = Math.max(from.atk - to.sld,1)
 	return damage
 }
+
+//fromがそれぞれtoの生きているキャラに攻撃する
+function attackAllCharacterToRandomTarget(from,to){
+	for(attacker of from){
+		log(attacker)
+		if(attacker.isDead){
+			continue
+		}
+		var target = selectRandomAliveCharacterIndex(to)
+		if(target === null){
+			continue
+		}
+		to[target].hp -= calcDamage(attacker,to[target])
+		if(to[target].hp <= 0){
+			to[target].isDead = true
+		}
+	}
+
+}
+
+//1ターン分の処理を行う
+function take1turn(enemies,allies){
+	//敵攻撃
+	attackAllCharacterToRandomTarget(enemies,allies)
+
+	//味方反撃
+	attackAllCharacterToRandomTarget(allies,enemies)
+}
+
+//敵一覧・味方一覧から生きているキャラをランダムに一人選択して添字を返す
+function selectRandomAliveCharacterIndex(charactersArray){
+	var aliveCharacters = listupAliveCharacter(charactersArray)
+	if(aliveCharacters.length == 0){
+		//return undefined を書いてはいけない以上
+		//こうしたけど、なんか直感的にバッドプラクティスなコードに見えてよくない
+		return null 
+	}
+
+	var idx = randInt(0,aliveCharacters.length-1)
+	var selected = aliveCharacters[idx]
+	return selected
+}
+
+//敵一覧・味方一覧から生きているキャラのインデックスのリストを返す
+function listupAliveCharacter(charactersArray){
+	var idxes = []
+	for(var i=0;i<charactersArray.length;++i){
+		if(!charactersArray[i].isDead){
+			idxes.push(i)
+		}
+	}
+	return idxes
+}
+
 
 //バトル処理を行う
 function processBattle(){
@@ -63,37 +119,16 @@ function processBattle(){
 	allies.push(new Ally("siro"))
 	allies.push(new Ally("kuro"))
 
+	var turnCount = 1
 	//敵と味方どちらかが全滅すると終了
-	while(enemies.length>0 && allies.length>0){
+	while(listupAliveCharacter(enemies).length > 0 && listupAliveCharacter(allies).length > 0 && turnCount	< 10){
 		log("**ターン開始**")
-		console.log(enemies)
-		console.log(allies)
-		for(var en of enemies){
-				if(allies.length == 0){
-					continue		
-				}
-				var target = randInt(0,allies.length-1)
-				allies[target].hp -= calcDamage(en,allies[target])
-				if(allies[target].hp <= 0){
-					log("**味方死亡**")
-					allies.splice(target,1)
-				}
-		}
-		for(var al of allies){
-				if(enemies.length == 0){
-					continue		
-				}
-				var target = randInt(0,enemies.length-1)
-				enemies[target].hp -= calcDamage(al,enemies[target])
-				if(enemies[target].hp <= 0){
-					log("**敵死亡**")
-					enemies.splice(target,1)
-				}
-		}
+		take1turn(enemies,allies)
+		turnCount++
 	}
 
-	save.status.siro.hp = allies[0].hp
-	save.status.kuro.hp = allies[1].hp
+	save.status.siro.hp = Math.max(allies[0].hp,0)
+	save.status.kuro.hp = Math.max(allies[1].hp,0)
 	updateCurrentHP()
 
 }
