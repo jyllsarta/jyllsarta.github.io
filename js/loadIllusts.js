0f3becc9e40c4235a65d69a1af255f057053d2c6
Jyll Sarta <illstarred.dive@gmail.com>
2018-01-31 12:26:22 +0900

    お仕事欄更新 / イラスト追加 / トップページの無駄なマークアップを削除

--- js/loadIllusts.js
+++ js/loadIllusts.js
@@ -1,5 +1,5 @@
 function loadIllusts(){
-	var count = 124
+	var count = 125
 	for(var i=count; i>0; --i){
 		$("#illustBox").append(`<a href="images/illust_all/${i}.png"><div class="thumb"></div>`)
 		$(".thumb:last").css("background-image",`url("images/illust_thumbnails/${i}.png")`)
